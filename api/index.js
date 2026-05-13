ad030763bf66f713d3c402f6136640c760c532c2
Francisco Affonso <faffonso@toolboxtve.com>
2026-05-13 10:47:58 -0300

    feat: draft de rutas

--- api/index.js
+++ api/index.js
@@ -4,9 +4,19 @@ const path = require('path');
 const swaggerJsdoc = require('swagger-jsdoc');
 const { name, version } = require('../package.json');
 
+const calendarioRouter = require('./routes/calendario');
+const carrerasRouter = require('./routes/carreras');
+const eventosRouter = require('./routes/eventos');
+const notificacionesRouter = require('./routes/notificaciones');
+const novedadesRouter = require('./routes/novedades');
+const perfilesRouter = require('./routes/perfiles');
+const reglamentacionRouter = require('./routes/reglamentacion');
+const usuariosRouter = require('./routes/usuarios');
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 // Serve robots.txt from project root
 app.get('/robots.txt', (req, res) => {
@@ -25,7 +35,7 @@ const swaggerDefinition = {
 
 const specs = swaggerJsdoc({
   definition: swaggerDefinition,
-  apis: [__filename]
+  apis: [__filename, path.join(__dirname, 'routes', '*.js')]
 });
 
 // Serve OpenAPI spec as JSON
@@ -87,6 +97,15 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello from centro-de-estudiantes-api!' });
 });
+
+app.use('/calendario', calendarioRouter);
+app.use('/carreras', carrerasRouter);
+app.use('/eventos', eventosRouter);
+app.use('/notificaciones', notificacionesRouter);
+app.use('/novedades', novedadesRouter);
+app.use('/perfiles', perfilesRouter);
+app.use('/reglamentacion', reglamentacionRouter);
+app.use('/usuarios', usuariosRouter);
 
 module.exports = app;
 
