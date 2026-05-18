d423b5237247e6db34694ff31aae1fdaa1dd0e83
upside-down mind <francisco.affonso@gmail.com>
2026-05-18 19:58:10 -0300

    Change access level from private to public

--- api/dbs/mockdb.js
+++ api/dbs/mockdb.js
@@ -22,7 +22,7 @@ module.exports = {
     async _updateFile(user, entity, data) {
         try {
             await put(`${BLOB_PREFIX}/${user}/${entity}.json`, JSON.stringify(data), {
-                access: 'private',
+                access: 'public',
                 allowOverwrite: true,
             });
         } catch (e) {
